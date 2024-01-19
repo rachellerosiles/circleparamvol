e9a5fcee7a6d778f90dba9917e7945fb48a23da9
Rachelle <rrosiles@hawk.iit.edu>
2024-01-19 15:43:05 -0600

    Initial Commit

---
+++ circleparamvol/circleparamvolApp.swift
@@ -0,0 +1,17 @@
+//
+//  circleparamvolApp.swift
+//  circleparamvol
+//
+//  Created by PHYS 440 Rachelle on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct circleparamvolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
